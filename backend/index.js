f2c1f4bc8ba0e63ea9d40461a2029dbd9ed7f992
JACKSON POULOSE <141388076+jacksonpoulose@users.noreply.github.com>
2025-03-20 10:32:49 +0000

    backend connection

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,13 @@
 const express = require("express");
-const path = require("path");
-const dotenv = require("dotenv");
+const cors = require("cors");
 
 const app = express();
 
-dotenv.config();
+app.use(cors()); 
+app.use(express.json());
 
-app.use(express.static(path.join(__dirname,'public')))
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+app.get("/api/test", (req, res) => {
+  res.json({ message: "backend is working!" });
 });
 
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, ()=>{
-    console.log("server running on port")
-})
-  
+app.listen(3000, () => console.log("Server running on port 3000"));
