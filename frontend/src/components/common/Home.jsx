af3436fcb1140eed8c8a9a3bcac221c3556d749c
Fathima Muhsina <fathimatmuhsina@gmail.com>
2025-03-23 13:16:16 +0530

    tailwind working

---
+++ frontend/src/components/common/Home.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+
+
+const Home = () => {
+  
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-200">
+    <h1 className="text-4xl font-bold text-blue-600">Tailwind is Working! 🎉</h1>
+  </div>
+  );
+};
+
+export default Home;
