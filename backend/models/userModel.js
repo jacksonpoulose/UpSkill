52588dc1851f3d62c2ba82b3a74be475a33bcb0c
JACKSON POULOSE <141388076+jacksonpoulose@users.noreply.github.com>
2025-03-25 08:13:21 +0000

    registration and login with Joi validations

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,19 +2,13 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
-    name: {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: {
       type: String,
-      required: true,
-      trim: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
+      enum: ["admin", "student", "mentor"],
+      default: "student",
     },
   },
   { timestamps: true }
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const connectDB = require("./config/db.js");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-const authRoutes = require("./routes/authRoutes.js")
+const authRoutes = require("./routes/authRoutes.js");
 
 const app = express();
 
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.use("/api/v1/auth", authRoutes)
+app.use("/api/v1/auth", authRoutes);
 
 app.get("/api/test", (req, res) => {
   res.json({ message: "backend is working!" });
