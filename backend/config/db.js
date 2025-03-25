4cb976aa31b86e7e973a6b30baaf9d091c0d8d6d
Fathima Muhsina <fathimatmuhsina@gmail.com>
2025-03-25 20:36:10 +0530

    second commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`connected to mongoDB ${conn.connection.host}`);
-  } catch {
+  } catch(error) {
     console.error(`error in mongoDB ${error.message}`);
     process.exit(1);
   }
