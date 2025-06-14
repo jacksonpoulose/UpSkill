a69cc524d951535ea71520e18e89134f0be01aa9
JACKSON POULOSE <141388076+jacksonpoulose@users.noreply.github.com>
2025-06-14 10:11:31 +0100

    student registration

--- backend/controllers/user/userController.js
+++ backend/controllers/user/userController.js
@@ -1,5 +1,6 @@
 const Notification = require("../../models/notificationModel");
 const MentorProfile = require("../../models/mentorProfile");
+const StudentProfile = require("../../models/studentProfile");
 const User = require("../../models/userModel");
 
 const mentorRegistration = async (req, res) => {
@@ -51,6 +52,84 @@ const mentorRegistration = async (req, res) => {
   }
 };
 
+const studentRegistration = async (req, res) => {
+  try {
+    const {
+      phoneNumber,
+      address,
+      city,
+      state,
+      country,
+      zipCode,
+      course,
+      learningGoals,
+      skillLevel,
+      resumeLink,
+      githubLink,
+      portfolioLink,
+      guardianName,
+      guardianPhoneNumber,
+      guardianEmail,
+      guardianRelationship,
+    } = req.body;
+    const userId = req.user?.id;
+    const userRole = req.user?.role;
 
+    if (!userId) {
+      return res.status(400).json({ message: "User ID missing from request" });
+    }
 
-module.exports = { mentorRegistration };
+    const studentProfile = new StudentProfile({
+      userId,
+      phoneNumber,
+      address,
+      city,
+      state,
+      country,
+      zipCode,
+      course,
+      learningGoals,
+      skillLevel,
+      resumeLink,
+      githubLink,
+      portfolioLink,
+      guardianName,
+      guardianPhoneNumber,
+      guardianEmail,
+      guardianRelationship,
+    });
+
+  
+
+    await studentProfile.save();
+
+    res.status(201).redirect("http://localhost:3000/api/v1/user/payment");
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error in student registration" });
+  }
+};
+
+const getPayment = async (req, res) => {
+  try {
+    res.status(200).json({ message: "Payment page" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error in getPayment" });
+  }
+};
+const postPayment = async (req, res) => {
+
+  const { amount } = req.body;
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: 'usd',
+    });
+
+    res.send({ clientSecret: process.env.STRIPE_SECRET_KEY });
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+};
+module.exports = { mentorRegistration, studentRegistration, postPayment, getPayment };
